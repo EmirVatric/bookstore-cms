e3abe34b30b8bf9ad399d79efa5212327e60b2e4
EmirVatric <vatricemir@gmail.com>
2019-10-03 19:16:15 +0200

    forms

--- src/components/BooksForm.jsx
+++ src/components/BooksForm.jsx
@@ -1,16 +1,30 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions'
+import { connect } from 'react-redux'
 
 const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
 
-const BooksForm = () => {
-  return (
-    <div>
-      <input type="text" name='bookTitle' />
-      <select name="" id="">{categories.map((categorie) => <option key={categorie}>{categorie}</option>)}</select>
-      <input type="submit" />
-    </div>
-  );
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" name='bookTitle' />
+        <select name="" id="">{categories.map((categorie) => <option key={categorie}>{categorie}</option>)}</select>
+        <input type="submit" />
+      </div>
+    );
+  }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBook: (book) => dispatch(CREATE_BOOK(book)),
+    removeBook: (book) => dispatch(REMOVE_BOOK(book))
+  }
+}
 
-export default BooksForm;
+export default connect(null, mapDispatchToProps)(BooksForm)
