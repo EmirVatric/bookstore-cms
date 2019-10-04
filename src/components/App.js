07f0a9533e615af6ce4cf160f1296bc89333eb8b
abdellani <abdellani@gmail.com>
2019-10-04 16:32:51 +0100

    Convert app class to arrow fun

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,10 @@ import './App.css';
 import BookList from './BookList'// eslint-disable-line no-unused-vars
 import BooksForm from './BooksForm'// eslint-disable-line no-unused-vars
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-  render() {
-    let { books } = this.props
-    return (
+const App =()=>
       <div>
-        <BookList books={books} />
+        <BookList />
         <BooksForm />
       </div>
-    );
-  }
-}
 
+export default App;
